5269dc507a23d59483970a375083222237ad4ded
ephraim-kamau54 <ephraim.kamau@student.moringaschool.com>
2021-06-23 12:46:45 +0300

    Initial commits

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+
+
+class Config:
+
+    SECRET_KEY = '12345'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ephraim:junior54@localhost/pitchapp'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,4 @@
+# export MAIL_USERNAME='ephraimkamau54@gmail.com'
+# export MAIL_PASSWORD='kanjipaki54'
+
+python3 manage.py server
